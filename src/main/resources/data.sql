ad126a980a73cc6a4f31789fafc1a019b073740e
luifon <luifon@alunos.utfpr.edu.br>
2019-09-21 14:37:02 -0300

    criado arquivo de inserção de dados inicial com dados do primeiro CRUD.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Category (id_category, name_category) VALUES (1, 'masculino');
+INSERT INTO Category (id_category, name_category) VALUES (2, 'feminino');
+INSERT INTO Category (id_category, name_category) VALUES (3, 'unissex');
